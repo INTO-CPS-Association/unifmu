9f853ab399801110bdccdfc9ca5886707d7d6393
Christian Legaard <clegaard@outlook.com>
2020-11-10 18:26:35 +0100

    cleanup paths

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ from shutil import SameFileError
 import subprocess
 import os
 import sys
-from sys import platform
+from sys import executable, platform
 import platform
 
 
@@ -24,46 +24,45 @@ class Chdir:
         os.chdir(self.old_wd)
 
 
-def path_to_host_binary() -> Path:
-
-    binary_basename = "unifmu"
-
-    system_to_binary_inout_tuple = {
-        "Linux": (f"lib{binary_basename}.so", f"linux64/{binary_basename}.so"),
-        "Windows": (f"{binary_basename}.dll", f"win64/{binary_basename}.dll"),
-        "Darwin": (f"lib{binary_basename}.dylib", f"darwin64/{binary_basename}.dylib"),
-    }
-
-    input, output = system_to_binary_inout_tuple[platform.system()]
-    binary_in = Path.cwd() / "target" / "debug" / input
-    binary_out = Path.cwd() / "examples" / "python_fmu" / "binaries" / output
-
-    return binary_in, binary_out
-
-
-def path_to_c_executable() -> Path:
-
-    system_to_executable = {
-        "Linux": "integration_tests",
-        "Windows": "Debug/integration_tests.exe",
-        "Darwin": "integration_tests",
-    }
-
-    return (
-        Path.cwd()
-        / "tests"
-        / "c_tests"
-        / "build"
-        / system_to_executable[platform.system()]
-    )
-
-
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__file__)
 
     parser = argparse.ArgumentParser()
+
+    # ----------- here we resolve file paths -----------------------
+
+    s = platform.system()
+
+    integration_tests_executable = (
+        (
+            Path("tests/c_tests/build")
+            / {
+                "Windows": "Debug/integration_tests.exe",
+                "Linux": "integration_tests",
+                "Darwin": "integration_tests",
+            }[s]
+        )
+        .absolute()
+        .__fspath__()
+    )
+    print(integration_tests_executable)
+    binary_basename = "unifmu"
+
+    # note that lib prefix is removed
+    input, output = {
+        "Linux": (f"lib{binary_basename}.so", f"linux64/{binary_basename}.so"),
+        "Windows": (f"{binary_basename}.dll", f"win64/{binary_basename}.dll"),
+        "Darwin": (f"lib{binary_basename}.dylib", f"darwin64/{binary_basename}.dylib"),
+    }[s]
+
+    wrapper_in = (Path.cwd() / "target" / "debug" / input).__fspath__()
+    wrapper_out = (
+        Path.cwd() / "examples" / "python_fmu" / "binaries" / output
+    ).__fspath__()
+
+    # -------------- parse args -------------------------
 
     parser.add_argument(
         "--update-wrapper",
@@ -86,8 +85,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    library_in, library_out = path_to_host_binary()
-
     if args.update_wrapper:
 
         logger.info("building wrapper")
@@ -98,11 +95,11 @@ if __name__ == "__main__":
             sys.exit(-1)
 
         logger.info(
-            f"wrapper was build, copying from '{library_in}' to '{library_out}'"
+            f"wrapper was build, copying from '{wrapper_in}' to '{wrapper_out}'"
         )
 
         try:
-            shutil.copy(src=library_in, dst=library_out)
+            shutil.copy(src=wrapper_in, dst=wrapper_out)
         except SameFileError:
             pass
 
@@ -125,7 +122,8 @@ if __name__ == "__main__":
         with Chdir(build_dir):
             logger.info("configuring cmake")
             res = subprocess.Popen(
-                args=["cmake", "-DCMAKE_BUILD_TYPE=Debug", ".."]).wait()
+                args=["cmake", "-DCMAKE_BUILD_TYPE=Debug", ".."]
+            ).wait()
 
             if res != 0:
                 logger.error("unable to configure cmake")
@@ -141,11 +139,10 @@ if __name__ == "__main__":
         logger.info("running C integration tests")
 
         resources_uri = (
-            Path.cwd() / "examples/python_fmu/resources").as_uri()
-        test_executable = path_to_c_executable()
+            (Path.cwd() / "examples" / "python_fmu" / "resources").absolute().as_uri()
+        )
         res = subprocess.Popen(
-            args=[test_executable,
-                  library_out, resources_uri]
+            args=[integration_tests_executable, wrapper_out, resources_uri]
         ).wait()
 
         if res != 0:
