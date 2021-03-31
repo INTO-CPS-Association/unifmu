277fbf1bc79af4a1efbab127f4f88274011bdb10
Christian Legaard <clegaard@outlook.com>
2021-03-31 10:56:01 +0200

    Fix minor issues in docker windows deployment

--- tool/unifmu/resources/backends/docker/run.ps1
+++ tool/unifmu/resources/backends/docker/run.ps1
@@ -1,12 +1,13 @@
 # fmu identifier from model description
+copy ..\modelDescription.xml modelDescription.xml
 $str = Get-Content modelDescription.xml
 $expr = 'guid="[^"]+'
 $uid = [regex]::matches($str,$expr).value.substring(6)
 
-copy ..\modelDescription.xml modelDescription.xml
+
 
 # build image
 docker build -t $uid .
 
 # run container
-docker run --net=host --rm $uid $arg[0] $arg[1]
+docker run --net=host --rm $uid $args[0] $args[1]
