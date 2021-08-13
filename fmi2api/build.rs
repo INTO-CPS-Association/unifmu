e83c657d4939e0997dfdc2c2d3635ca9c01ca091
Christian Legaard <clegaard@outlook.com>
2021-08-13 22:51:44 +0200

    Remove auto generated assets from VC

--- fmi2api/build.rs
+++ fmi2api/build.rs
@@ -5,10 +5,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap();
 
     Ok(())
-
-    // tonic_build::configure()
-    //     .build_client(true)
-    //     .out_dir("src")
-    //     .compile(&["unifmu_fmi2.proto"], &["../../schemas"])?;
-    // Ok(())
 }
