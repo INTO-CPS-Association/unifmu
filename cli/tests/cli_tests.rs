55232fb82701f3ba26580fe2cb5bfde764478185
DisasterlyDisco <aleksander.r.m@protonmail.com>
2025-01-09 13:39:02 +0100

    Added communication breakage tests

--- cli/tests/cli_tests.rs
+++ cli/tests/cli_tests.rs
@@ -346,3 +346,107 @@ fn test_simulate_python_fmi2_distributed() {
 
     distributed_fmu_python_test(fmu, "fmi2_simulate");
 }
+
+#[test]
+#[should_panic(expected = "PYTHON TEST FAILED - fmi2_instantiate - instantiation: Failed to instantiate model")]
+fn test_unexpected_exit_in_handshake_csharp_fmi2_local() {
+    let fmu = LocalFmu::get_clone(
+        &FmiVersion::Fmi2, 
+        &FmuBackendImplementationLanguage::CSharp
+    );
+
+    fmu.break_model();
+
+    fmu_python_test(fmu, "fmi2_instantiate");
+}
+
+#[test]
+#[should_panic(expected = "PYTHON TEST FAILED - fmi2_instantiate - instantiation: Failed to instantiate model")]
+fn test_unexpected_exit_in_handshake_java_fmi2_local() {
+    let fmu = LocalFmu::get_clone(
+        &FmiVersion::Fmi2, 
+        &FmuBackendImplementationLanguage::Java
+    );
+
+    fmu.break_model();
+
+    fmu_python_test(fmu, "fmi2_instantiate");
+}
+
+#[test]
+#[should_panic(expected = "PYTHON TEST FAILED - fmi2_instantiate - instantiation: Failed to instantiate model")]
+fn test_unexpected_exit_in_handshake_python_fmi2_local() {
+    let fmu = LocalFmu::get_clone(
+        &FmiVersion::Fmi2, 
+        &FmuBackendImplementationLanguage::Python
+    );
+
+    fmu.break_model();
+
+    fmu_python_test(fmu, "fmi2_instantiate");
+}
+
+#[test]
+#[should_panic(expected = "PYTHON TEST FAILED - fmi3_instantiate - instantiation: Failed to instantiate FMU")]
+fn test_unexpected_exit_in_handshake_python_fmi3_local() {
+    let fmu = LocalFmu::get_clone(
+        &FmiVersion::Fmi3, 
+        &FmuBackendImplementationLanguage::Python
+    );
+
+    fmu.break_model();
+
+    fmu_python_test(fmu, "fmi3_instantiate");
+}
+
+#[test]
+#[should_panic(expected = "PYTHON TEST FAILED - fmi2_simulate: fmi2DoStep failed with status 3 (error).")]
+fn test_unexpected_exit_during_command_csharp_fmi2_local() {
+    let fmu = LocalFmu::get_clone(
+        &FmiVersion::Fmi2, 
+        &FmuBackendImplementationLanguage::CSharp
+    );
+
+    fmu.break_do_step_function();
+
+    fmu_python_test(fmu, "fmi2_simulate");
+}
+
+#[test]
+#[should_panic(expected = "PYTHON TEST FAILED - fmi2_simulate: fmi2DoStep failed with status 3 (error).")]
+fn test_unexpected_exit_during_command_java_fmi2_local() {
+    let fmu = LocalFmu::get_clone(
+        &FmiVersion::Fmi2, 
+        &FmuBackendImplementationLanguage::Java
+    );
+
+    fmu.break_do_step_function();
+
+    fmu_python_test(fmu, "fmi2_simulate");
+}
+
+#[test]
+#[should_panic(expected = "PYTHON TEST FAILED - fmi2_simulate: fmi2DoStep failed with status 3 (error).")]
+fn test_unexpected_exit_during_command_python_fmi2_local() {
+    let fmu = LocalFmu::get_clone(
+        &FmiVersion::Fmi2, 
+        &FmuBackendImplementationLanguage::Python
+    );
+
+    fmu.break_do_step_function();
+
+    fmu_python_test(fmu, "fmi2_simulate");
+}
+
+#[test]
+#[should_panic(expected = "PYTHON TEST FAILED - fmi3_simulate: fmi3DoStep failed with status 3 (error).")]
+fn test_unexpected_exit_during_command_python_fmi3_local() {
+    let fmu = LocalFmu::get_clone(
+        &FmiVersion::Fmi3, 
+        &FmuBackendImplementationLanguage::Python
+    );
+
+    fmu.break_do_step_function();
+
+    fmu_python_test(fmu, "fmi3_simulate");
+}
