bfcaa060639fdfc5cb596aad96d644b7d9f46d7a
DisasterlyDisco <aleksander.r.m@protonmail.com>
2025-01-14 13:10:53 +0100

    Removed unused commented code from spawn.rs

--- fmiapi/src/spawn.rs
+++ fmiapi/src/spawn.rs
@@ -76,61 +76,6 @@ impl LaunchConfig {
     }
 }
 
-/* 
-pub fn spawn_fmi2_slave(resource_path: &Path) -> Result<(Fmi2CommandDispatcher, Popen), ()> {
-
-    let launch_command = LaunchConfig::create(resource_path)?
-        .get_launch_command()?;
-
-    let mut dispatcher = Fmi2CommandDispatcher::new("tcp://127.0.0.1:0");
-
-    let endpoint = dispatcher.endpoint.to_owned();
-    let endpoint_port = match endpoint
-        .split(":")
-        .last() {
-            Some(port) => port,
-            None => {
-                error!("No port was specified for endpoint.");
-                return Err(());
-            }
-        }
-        .to_owned();
-
-    // set environment variables
-    let mut env_vars: Vec<(OsString, OsString)> = std::env::vars_os().collect();
-
-    env_vars.push((
-        OsString::from("UNIFMU_DISPATCHER_ENDPOINT"),
-        OsString::from(endpoint),
-    ));
-    env_vars.push((
-        OsString::from("UNIFMU_DISPATCHER_ENDPOINT_PORT"),
-        OsString::from(endpoint_port),
-    ));
-
-    // spawn process
-    let popen = match Popen::create(
-        &launch_command,
-        PopenConfig {
-            cwd: Some(resource_path.as_os_str().to_owned()),
-            env: Some(env_vars),
-            ..Default::default()
-        },
-    ) {
-        Ok(popen) => popen,
-        Err(_e) => {
-            eprintln!("Unable to start the process using the specified command '{:?}'. Ensure that you can invoke the command directly from a shell", launch_command);
-            return Err(());
-        }
-    };
-
-    match dispatcher.await_handshake() {
-        Ok(_handshake) => Ok((dispatcher, popen)),
-        Err(_e) => Err(()),
-    }
-}
-*/
-
 pub fn spawn_fmi2_slave(resource_path: &Path) -> Result<Dispatcher, ()> {
     let config = LaunchConfig::create(resource_path)?;
 
